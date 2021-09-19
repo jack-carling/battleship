c21bf46aea513462155ba45cf8fd547dfb1769ab
jack-carling <jack@carling.se>
2021-09-19 13:53:01 +0200

    Added logo
    • Made a logo and added to the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Game from './components/Game';
 
 function App() {
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,10 +1,12 @@
 import styles from '../styles/Game.module.scss';
+import logo from '../assets/logo.png';
 
 import Board from './Board';
 
 export default function Game() {
   return (
     <section className={styles.game}>
+      <img src={logo} className={styles.logo} alt="Logo" />
       <Board />
     </section>
   );
